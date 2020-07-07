e9771f961a01cc7789a1b50fd53950198f93e172
James Grieve <pocojames@hotmail.com>
2020-07-07 16:45:14 -0700

    cipher complete

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,27 @@
-class CaesarCipher
+    def caesar_cipher(input, factor)
+        unciphered_array = input.split("")
+        ciphered_array = []
 
-end
+        unciphered_array.each do |char| 
+        if char.upcase == char
+            ciphered_array << (shift_char(char.downcase, factor)).upcase
+        else
+            ciphered_array << shift_char(char, factor)
+        end  
+    end
+        ciphered_array.join("")
+    end
+
+    def shift_char(char, factor)
+        alphabet = ("a".."z").to_a
+        return char if !alphabet.include?(char)
+        unshifted_i = alphabet.find_index(char)
+        shifted_i = unshifted_i + factor
+        alphabet[shifted_i % 26] 
+    end
+    
+    #caesar_cipher("What a string!", 5)
+
+
+
+
